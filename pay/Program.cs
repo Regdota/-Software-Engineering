a76ac057fb3e4624d1faf267549e07cf80f9fa7e
Denis <Regdota2@gmail.com>
2016-02-18 22:10:49 +0400

    Update Program.cs

--- pay/Program.cs
+++ pay/Program.cs
@@ -4,33 +4,111 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace pay
+namespace kkkkk
 {
     class Program
     {
         static void Main(string[] args)
         {
-            float a = 0.05f, b = 0.1f, c = 10000, d = 6000, pay;
+            int a = 5, b = 10, c = 10000, d = 6000, r=100000, f= 15, g=300, y=100, sum;
+            int o, t, th, fo, fi, six, seven;
 
-            Console.WriteLine("Сумму выручки:");
-            pay = Convert.ToInt32(Console.ReadLine());
 
-            if (pay <= d)
+
+            Console.WriteLine("Понедельник:");
+            o = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Вторник:");
+            t = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Среда:");
+            th = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Четверг:");
+            fo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Пятница:");
+            fi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Суббота:");
+            six = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Воскресение:");
+            seven = Convert.ToInt32(Console.ReadLine());
+
+
+            int[] myArr = new int[7];
+
+            // Инициализируем каждый элемент массива вручную 
+            myArr[0] = o;
+            myArr[1] = t;
+            myArr[2] = th;
+            myArr[3] = fo;
+            myArr[4] = fi;
+            myArr[5] = six;
+            myArr[6] = seven;
+
+
+
+
+
+
+
+
+            for (int i = 0; i < 4; i++)
             {
-                Console.WriteLine("Зарплата работника: 300");
+
+                if (myArr[i] <= d)
+                {
+                    myArr[i] = g;
+                }
+                if (myArr[i] <= c)
+                {
+                    myArr[i] = myArr[i] * a / y;
+                }               
+                if (myArr[i] > c)
+                {
+                    myArr[i] = (myArr[i] - c) * b / y + c * a / y;
+                }
+                if (myArr[i]>r)
+                {
+                    myArr[i] = myArr[i] * f / y;
+                }
+
             }
-            else if (pay <= c)
+
+
+
+            /*-------------------------*/
+            for (int i = 5; i < 6; i++)
             {
-                Console.WriteLine("Зарплата работника:");
-                Console.WriteLine(pay * a );
+
+                if (myArr[i] <= d)
+                {
+                    myArr[i] = g * 2;
+                }
+                if (myArr[i] <= c)
+                {
+                    myArr[i] = myArr[i] * a / y * 2;
+                }
+                if (myArr[i] > c)
+                {
+                    myArr[i] = (myArr[i] - c) * b / y * 2 + c * a / y * 2;
+                }
+                if (myArr[i] > r)
+                {
+                    myArr[i] = myArr[i] * f / y * 2;
+                }
+
+               
             }
-            else
-            {
-                Console.WriteLine("Зарплата работника:");
-                Console.WriteLine((pay - c) * b + c * a);
-            }
-            Console.WriteLine("Нажмите ENTER что бы выйти из программы");
+            /*-------------*/
+
+            /*Не правильно работает...*/
+            Console.WriteLine("Зарплата: " + myArr.Sum());
+            
+                
+  
+            
+          
+
             Console.ReadKey();
+
+
         }
     }
 }
